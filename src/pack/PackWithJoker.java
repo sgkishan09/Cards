f69f1ffca784ac5e606af9edd749a6e9599f957a
gkishan <gkishan@L-MAA-25005033.paypalcorp.com>
2016-08-09 18:23:54 +0530

    Customised PackWithJoker

--- src/pack/PackWithJoker.java
+++ src/pack/PackWithJoker.java
@@ -1,17 +1,21 @@
 package pack;
+
 import java.util.ArrayList;
 
 import entities.Card;
 
 public class PackWithJoker extends Pack {
 	public PackWithJoker() {
+		this(2);
+	}
+
+	public PackWithJoker(int n) {
 		cards = new ArrayList<>();
 		for (String suit : Card.SUITS)
 			for (String face : Card.FACES)
 				cards.add(new Card(face, suit));
-		cards.add(Card.JOKER);
-		cards.add(Card.JOKER);
-
+		while (n-- > 0)
+			this.cards.add(Card.JOKER);
 	}
 
 }
