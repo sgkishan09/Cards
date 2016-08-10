9bae66c1cefd7512b457ba63b69c92a13e07287f
Vikram <ak.vikram007@gmail.com>
2016-08-10 13:41:15 +0530

    Added Player and driver classes

--- src/diamond/player/HumanPlayer.java
+++ src/diamond/player/HumanPlayer.java
@@ -1,5 +1,34 @@
 package diamond.player;
 
-public class HumanPlayer {
+import entities.*;
+import java.util.*;
+
+public class HumanPlayer extends Player{
+	private ArrayList<Card> cards;
+	private int points;
+
+	public Card bid() {
+		String suit, face;
+		Card toBid;
+		Scanner in = new Scanner(System.in);
+		System.out.println("-----List of cards available-----");
+		System.out.println(cards);
+		System.out.println("Choose card to bid:");
+		System.out.println("Suit:");
+		suit = in.nextLine();
+		System.out.println("Face:");
+		face = in.nextLine();
+		toBid = new Card(face, suit);
+		this.removeCard(toBid);
+		return toBid;
+	}
+
+	private void removeCard(Card c) {
+		cards.remove(c);
+	}
+
+	public void feedbackOfCurrentBid(Double score, Card diamond, Collection c){
+		System.out.println("Score: "+ score + "\nDiamond Card: "+ diamond +"\nCards Played: "+ c);
+	}
 
 }
--- src/diamond/player/Player.java
+++ src/diamond/player/Player.java
@@ -1,5 +1,17 @@
 package diamond.player;
 
-public interface Player {
+import java.util.Collection;
+
+import entities.Card;
+
+public abstract class Player {
+
+	public Card bid(){
+		return null;
+	};
+	
+	public void feedbackOfCurrentBid(Double score, Card card, Collection c){
+		
+	};
 
 }
